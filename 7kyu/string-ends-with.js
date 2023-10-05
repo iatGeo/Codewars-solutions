7875be1db1c277bb64d3b404520a18d06166a0e3
iatGeo <81882416+iatGeo@users.noreply.github.com>
2023-10-05 20:03:48 +0300

    more solutions

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,7 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+
+//My solution
+function solution(str, ending){
+    return str.endsWith(ending)
+}
