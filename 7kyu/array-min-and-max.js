5b2ddb59d4268fb0a5de91fd10c3b59d0fc4f3bf
iatGeo <g.iatridis@protonmail.com>
2023-10-31 22:04:08 +0200

    more solutions

---
+++ 7kyu/array-min-and-max.js
@@ -0,0 +1,8 @@
+// The highest profit wins!
+// Write a function that returns both the minimum and maximum number of the given list/array.
+
+
+//My solution
+function minMax(arr){
+    return [Math.min(...arr), Math.max(...arr)]
+}
