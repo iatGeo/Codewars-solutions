5701835af2686d5f7907a3db525986ca46fad79f
iatGeo <g.iatridis@protonmail.com>
2023-11-07 21:03:27 +0200

    more solutions

---
+++ 8kyu/squaring-an-argument.js
@@ -0,0 +1,6 @@
+// Function 2 - squaring an argument
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+//My solution
+const square = arg => Math.pow(arg,2)
