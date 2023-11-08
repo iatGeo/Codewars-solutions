37c2849dc09bf9741766492b90160e2135edfffc
iatGeo <g.iatridis@protonmail.com>
2023-11-08 16:56:21 +0200

    more solutions

---
+++ 8kyu/sum-two-strings.js
@@ -0,0 +1,9 @@
+// Sum The Strings
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string)
+// If either input is an empty string, consider it as zero.
+
+
+//My solution
+function sumStr(a,b) {
+    return String( Number(a) + Number(b) )
+}
