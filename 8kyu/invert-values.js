0fea02b55304d8a02f13087d0cf89e175cdbf045
iatGeo <g.iatridis@protonmail.com>
2023-11-13 10:59:00 +0200

    more solutions

---
+++ 8kyu/invert-values.js
@@ -0,0 +1,6 @@
+// Invert values
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+
+//My solution
+const invert = array => array.map(elem => -elem)
