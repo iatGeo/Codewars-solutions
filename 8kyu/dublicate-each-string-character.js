ddfd3be5e5677af7b7d8c385e359989b655ac4ac
iatGeo <g.iatridis@protonmail.com>
2023-11-14 10:19:32 +0200

    more solutions

---
+++ 8kyu/dublicate-each-string-character.js
@@ -0,0 +1,12 @@
+// Double Char
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+
+//My solution
+function doubleChar(str) {
+    return str.split('').map(elem => elem.repeat(2)).join('')
+}
