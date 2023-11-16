ea19a2d35f5ab56d73312570c79ced7250c8e6a2
iatGeo <g.iatridis@protonmail.com>
2023-11-16 23:09:21 +0200

    more solutions

---
+++ 8kyu/exc-lost-without-map.js
@@ -0,0 +1,8 @@
+// Beginner - Lost Without a Map
+// Given an array of integers, return a new array with each value doubled.
+
+
+//My solution
+function maps(x){
+    return x.map(elem => 2*elem)
+}
