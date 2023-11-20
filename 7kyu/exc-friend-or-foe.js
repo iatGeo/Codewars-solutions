124b3fd01a642884d40bfec00d07cc13d586080b
iatGeo <g.iatridis@protonmail.com>
2023-11-20 14:20:28 +0200

    more solutions

---
+++ 7kyu/exc-friend-or-foe.js
@@ -0,0 +1,9 @@
+// Friend or Foe?
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+
+//My solution
+function friend(friends){
+    return friends.filter( elem => elem.length == 4 )
+}
