523ebf6fc4521f1c9c8af3da48c214f6e3ad172e
iatGeo <g.iatridis@protonmail.com>
2023-11-21 16:13:50 +0200

    more solutions

---
+++ 8kyu/remove-exclamation-marks.js
@@ -0,0 +1,8 @@
+// Remove exclamation marks
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+//My solution
+function removeExclamationMarks(s) {
+    return s.replace(/!/g, '')
+}
