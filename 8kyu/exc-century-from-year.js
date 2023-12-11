f86c87702456875bd3d57739f55e84c6b53d9dbe
iatGeo <g.iatridis@protonmail.com>
2023-12-11 20:40:18 +0200

    more solutions

---
+++ 8kyu/exc-century-from-year.js
@@ -0,0 +1,21 @@
+// Century From Year
+// Given a year, return the century it is in.
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+
+//My solutions
+function century(year) {
+    let x = String(year)
+    
+    if(x.length<=2){
+      return 1
+    }else{
+      return x.slice(-2) == 00 ?  +x.slice(0,-2) : +x.slice(0,-2) +1
+    }
+}
+
+const century = year => Math.ceil(year/100)
