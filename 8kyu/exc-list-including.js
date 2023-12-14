ddbc757f11370cc5b98cbd5783dba811832a95fc
iatGeo <g.iatridis@protonmail.com>
2023-12-14 23:07:14 +0200

    more solutions

---
+++ 8kyu/exc-list-including.js
@@ -0,0 +1,8 @@
+// Enumerable Magic - Does My List Include This?
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+//My solution
+function include(arr, item){
+    return arr.includes(item)
+}
