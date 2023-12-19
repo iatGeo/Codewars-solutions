56f10243a356172218e8b33245fa667899d49138
iatGeo <g.iatridis@protonmail.com>
2023-12-19 17:42:20 +0200

    more solutions

---
+++ 8kyu/take-first-n-elements.js
@@ -0,0 +1,8 @@
+// Enumerable Magic #25 - Take the First N Elements
+// Create a function that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+
+
+//My solution
+function take(arr, n) {
+    return arr.splice(0, n)
+}
