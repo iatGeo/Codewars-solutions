f335e971ea4e7b742da7604df891833819ef5792
iatGeo <g.iatridis@protonmail.com>
2023-12-26 22:56:16 +0200

    more solutions

---
+++ 7kyu/how-is-the-array-sorted.js
@@ -0,0 +1,16 @@
+// Sorted? yes? no? how?
+// Complete the method which accepts an array of integers, and returns one of the following:
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+
+//My solution
+function isSortedAndHow(array) {
+    const testing = array.join('')
+    const ascending = array.sort( (a,b) => a-b).join('')
+    const descending = array.sort( (a,b) => b-a).join('')
+    
+    return testing===ascending ? "yes, ascending" : testing===descending ? "yes, descending" : "no"
+}
