6f5756f3b38fbe2c5ac2b117dfbc1fab53b821a0
iatGeo <g.iatridis@protonmail.com>
2023-12-28 16:00:22 +0200

    more solutions

---
+++ 8kyu/exc-clock.js
@@ -0,0 +1,19 @@
+// Beginner Series #2 Clock
+// Clock shows h hours, m minutes and s seconds after midnight.
+// Your task is to write a function which returns the time since midnight in milliseconds.
+// Example:
+// h = 0
+// m = 1
+// s = 1
+// result = 61000
+// Input constraints:
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+
+//My solution
+function past(h, m, s){
+    let seconds = h*3600 + m*60 + s
+    return 1000*seconds
+}
