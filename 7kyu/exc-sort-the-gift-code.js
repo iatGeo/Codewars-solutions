2bef2e91b7b2d7dbadf7896cdefde0071a99f12d
iatGeo <g.iatridis@protonmail.com>
2024-01-01 21:36:12 +0200

    more solutions

---
+++ 7kyu/exc-sort-the-gift-code.js
@@ -0,0 +1,12 @@
+// Sort the Gift Code
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+// Examples (Input -- => Output):
+// "abcdef"                      -- => "abcdef"
+// "pqksuvy"                     -- => "kpqsuvy"
+// "zyxwvutsrqponmlkjihgfedcba"  -- => "abcdefghijklmnopqrstuvwxyz"
+
+
+//My solution
+function sortGiftCode(code){
+    return code.split('').sort().join('')
+}
