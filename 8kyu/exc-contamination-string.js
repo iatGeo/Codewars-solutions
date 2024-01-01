00c58f91163126ac8e38a3452a284b6389ff8ae9
iatGeo <g.iatridis@protonmail.com>
2024-01-01 21:33:18 +0200

    more solutions

---
+++ 8kyu/exc-contamination-string.js
@@ -0,0 +1,16 @@
+// Contamination #1 -String-
+// An AI has infected a text with a character!!
+// This text is now fully mutated to this character.
+// If the text or the character are empty, return an empty string.
+// There will never be a case when both are empty as nothing is going on!!
+// Note: The character is a string of length 1 or an empty string.
+// Example
+// text before = "abc"
+// character   = "z"
+// text after  = "zzz"
+
+
+//My solution
+function contamination(text, char){
+    return char.repeat(text.length)
+}
