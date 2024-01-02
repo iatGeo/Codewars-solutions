9e0e278abe2e18445b919703630cae0945096a67
iatGeo <g.iatridis@protonmail.com>
2024-01-02 23:13:24 +0200

    more solutions

---
+++ 7kyu/exc-no-oddities-here.js
@@ -0,0 +1,9 @@
+// No oddities here
+// Write a small function that returns the values of an array that are not odd.
+// All values in the array will be integers. Return the good values in the order they are given.
+
+
+//My solution
+function noOdds( values ){
+    return values.filter( int => int%2===0)
+}
