2d36462c8fe6f730928080294142f6f4578861d6
iatGeo <g.iatridis@protonmail.com>
2024-01-08 13:20:03 +0200

    more solutions

---
+++ 8kyu/add-item-to-array.js
@@ -0,0 +1,9 @@
+// Basic Training: Add item to an Array
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+// The websites array has already been defined for you using the following code:
+// var websites = []
+
+
+//My solution
+websites.push("codewars")
