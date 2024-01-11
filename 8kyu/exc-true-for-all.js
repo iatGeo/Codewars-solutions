03bc94cd4cdc2bb1e6da09b7c3e326ae7481f5e7
iatGeo <g.iatridis@protonmail.com>
2024-01-11 13:14:43 +0200

    more solutions

---
+++ 8kyu/exc-true-for-all.js
@@ -0,0 +1,11 @@
+// Enumerable Magic #1 - True for All?
+// Create a method all which takes two params:
+// a sequence
+// a function (function pointer in C)
+// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+
+
+//My solution
+function all( arr, fun ){
+    return arr.every(fun)
+}
