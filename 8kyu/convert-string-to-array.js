70dff13dab9b5777d27a86876dfed7a05f064253
iatGeo <g.iatridis@protonmail.com>
2024-01-15 15:22:12 +0200

    more solutions

---
+++ 8kyu/convert-string-to-array.js
@@ -0,0 +1,8 @@
+// Convert a string to an array
+// Write a function to split a string and convert it into an array of words.
+
+
+//My solution
+function stringToArray(string){
+    return string.split(' ')
+}
