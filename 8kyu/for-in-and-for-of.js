834a88834f802923674d8461a045640322da5386
iatGeo <g.iatridis@protonmail.com>
2024-01-16 12:37:16 +0200

    more solutions

---
+++ 8kyu/for-in-and-for-of.js
@@ -0,0 +1,16 @@
+// Training JS #12: loop statement --for..in and for..of
+// The function giveMeFive accepts 1 parameter, obj, which is an object.
+// Create an array (which you will eventually return). Then, traverse obj, checking each key and value. If the length of the key is equal to 5, then push the key to your array. Separately, if the length of the value is equal to 5, then push the value to your array.
+// At the end, return your array.
+// You should use for..in in your code, otherwise your solution may not pass this kata.
+
+
+//My solution
+function giveMeFive(obj){
+    let results = []
+    for(let key in obj){
+      key.length===5 ? results.push(key) : null
+      obj[key].length===5 ? results.push(obj[key]) : null
+    }
+    return results
+}
