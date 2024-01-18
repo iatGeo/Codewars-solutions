949a0658dfe226eed46410bba61abe1fd13c634c
iatGeo <g.iatridis@protonmail.com>
2024-01-18 21:17:01 +0200

    ''more''

---
+++ 8kyu/max-array-elems-difference.js
@@ -0,0 +1,13 @@
+// max diff - easy
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+
+
+//My solution
+function maxDiff(list){
+    return list.length <= 1 ? 0 : Math.max(...list) - Math.min(...list)
+}
