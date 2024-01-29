a5299ffa9682258e7082dc8ef32376b8f565b92b
iatGeo <g.iatridis@protonmail.com>
2024-01-29 18:45:18 +0200

    more solutions

---
+++ 8kyu/exc-bug-squashing.js
@@ -0,0 +1,24 @@
+// Grasshopper - Bug Squashing
+// You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+// You are using a library that already has the functions below. Create a function named main that calls the functions in the proper order.
+// - combat
+// - buyHealth
+// - getCoins
+// - printStatus
+// - rollDice
+// - move
+
+
+//My solution
+let health = 100
+let position = 0
+let coins = 0
+
+function main () {
+  rollDice()
+  move()
+  combat()
+  getCoins()
+  buyHealth()
+  printStatus()
+}
