163c0bd864db37245ffbc402ede663310b691286
iatGeo <g.iatridis@protonmail.com>
2024-02-15 21:49:54 +0200

    more solutions

---
+++ 8kyu/parse-float.js
@@ -0,0 +1,12 @@
+// Parse float
+// Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
+
+
+//My solution
+function parseF(s){
+    if( typeof +s==='number' && typeof s !== 'boolean' ){
+      return isNaN(+s) ? null : +s
+    }else{
+      return null
+    }
+}
