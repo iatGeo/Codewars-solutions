81c65f4b29cda618d06d9da8c8734c07f011e555
iatGeo <g.iatridis@protonmail.com>
2024-02-23 22:26:13 +0200

    more solutions

---
+++ 8kyu/exc-kata-example-twist.js
@@ -0,0 +1,7 @@
+// Kata Example Twist
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+// Add the value "codewars" to the array websites 1,000 times.
+
+
+//My solution
+let websites = new Array(1000).fill('codewars')
