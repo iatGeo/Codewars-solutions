4281f98923158b57e07469c49a8365c5f4295212
iatGeo <g.iatridis@protonmail.com>
2024-03-01 23:19:49 +0200

    more solutions

---
+++ 6kyu/extract-portion-of-file-name.js
@@ -0,0 +1,28 @@
+// extract portion of file name
+// DESCRIPTION:
+// You have to extract a portion of the file name as follows:
+// Assume it will start with date represented as long number
+// Followed by an underscore
+// You'll have then a filename with an extension
+// it will always have an extra extension at the end
+// Inputs:
+// 1231231223123131_FILE_NAME.EXTENSION.OTHEREXTENSION
+// 1_This_is_an_otherExample.mpg.OTHEREXTENSIONadasdassdassds34
+// 1231231223123131_myFile.tar.gz2
+// Outputs
+// FILE_NAME.EXTENSION
+// This_is_an_otherExample.mpg
+// myFile.tar
+// Acceptable characters for random tests:
+// abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-0123456789
+
+
+//My solution
+class FileNameExtractor {
+    static extractFileName (dirtyFileName) {
+        const numSlice = dirtyFileName.indexOf('_')
+        const extSlice = dirtyFileName.lastIndexOf('.')
+        dirtyFileName = dirtyFileName.slice(numSlice+1, extSlice)
+        return dirtyFileName
+    }
+}
