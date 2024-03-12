4417bc4ed7deefef8d0f3c5118b574aaccd755c9
iatGeo <g.iatridis@protonmail.com>
2024-03-12 16:20:22 +0200

    more solutions

---
+++ 7kyu/sort-arrays-minus-1.js
@@ -0,0 +1,11 @@
+// Sort arrays - 1
+// DESCRIPTION:
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+//My solution
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort()
+}
