3bdd9b005559fc01ac9776a97d44cd550dfae585
iatGeo <g.iatridis@protonmail.com>
2024-03-18 22:32:27 +0200

    more solutions

---
+++ 7kyu/how-many-arguments.js
@@ -0,0 +1,10 @@
+// How many arguments
+// DESCRIPTION:
+// Write a function that returns the number of arguments it received.
+// args_count() --> 0
+// args_count('a') --> 1
+// args_count('a', 'b') --> 2
+
+
+//My solution
+const args_count = (...arguments) => arguments.length
