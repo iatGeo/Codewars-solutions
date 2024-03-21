5249275200feb93c95c33e8e30e0085bb450375a
iatGeo <g.iatridis@protonmail.com>
2024-03-21 16:23:33 +0200

    more solutions

---
+++ 7kyu/exc-strong-nums.js
@@ -0,0 +1,36 @@
+// Strong Number (Special Numbers Series #2)
+// DESCRIPTION:
+// Definition
+// Strong number is the number that the sum of the factorial of its digits is equal to number itself.
+// For example, 145 is strong, since 1! + 4! + 5! = 1 + 24 + 120 = 145.
+// Task
+// Given a number, Find if it is Strong or not and return either "STRONG!!!!" or "Not Strong !!".
+// Notes
+// Number passed is always Positive.
+// Return the result as String
+// Input >> Output Examples
+// strong_num(1) ==> return "STRONG!!!!"
+// Since, the sum of its digits' factorial (1) is equal to number itself, then its a Strong.
+// strong_num(123) ==> return "Not Strong !!"
+// Since the sum of its digits' factorial of 1! + 2! + 3! = 9 is not equal to number itself, then it's Not Strong .
+// strong_num(2)  ==>  return "STRONG!!!!"
+// Since the sum of its digits' factorial of 2! = 2 is equal to number itself, then its a Strong.
+// strong_num(150) ==> return "Not Strong !!"
+// Since the sum of its digits' factorial of 1! + 5! + 0! = 122 is not equal to number itself, Then it's Not Strong .
+
+//My solution
+function strong(n) {
+   function getFactorial(num) {
+      let result = 1;
+      for (let i = 1; i <= num; i++) {
+         result *= i;
+      }
+      return result;
+   }
+
+   const test = String(n)
+      .split("")
+      .map((num) => getFactorial(+num))
+      .reduce((a, b) => a + b, 0);
+   return test === n ? "STRONG!!!!" : "Not Strong !!";
+}
