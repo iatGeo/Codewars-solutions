691286e42a786ac0f102a3da6f0693c03ac41355
iatGeo <g.iatridis@protonmail.com>
2024-03-28 22:44:08 +0200

    more solutions

---
+++ 8kyu/merging-sorted-arrays-without-duplicates.js
@@ -0,0 +1,8 @@
+// Merging sorted integer arrays (without duplicates)
+// DESCRIPTION:
+// Write a function that merges two sorted arrays into a single one. The arrays only contain integers. Also, the final outcome must be sorted and not have any duplicate.
+
+//My solution
+function mergeArrays(a, b) {
+   return [...new Set(a.concat(b))].sort((a, b) => a - b);
+}
