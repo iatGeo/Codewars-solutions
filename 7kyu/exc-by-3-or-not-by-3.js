7c83a696cee068f13cdd7eba5966b38d4933deec
iatGeo <g.iatridis@protonmail.com>
2024-04-01 23:00:23 +0300

    more solutions

---
+++ 7kyu/exc-by-3-or-not-by-3.js
@@ -0,0 +1,15 @@
+// By 3, or not by 3? That is the question . . .
+// DESCRIPTION:
+// A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+// Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+// Example:
+// "123"      -> true
+// "8409"     -> true
+// "100853"   -> false
+// "33333333" -> true
+// "7"        -> false
+
+//My solution
+function divisibleByThree(str) {
+   return [...str].reduce((a, b) => +b + a, 0) % 3 === 0;
+}
