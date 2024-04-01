232941502c2d73b1a98572662dc88809137dd0db
iatGeo <g.iatridis@protonmail.com>
2024-04-01 22:58:52 +0300

    more solutions

---
+++ 7kyu/exc-make-backronym.js
@@ -0,0 +1,20 @@
+// makeBackronym
+// DESCRIPTION:
+// back·ro·nym
+// An acronym deliberately formed from a phrase whose initial letters spell out a particular word or words, either to create a memorable name or as a fanciful explanation of a word's origin.
+// Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, using the preloaded dictionary and return a string of words, separated with a single space (but no trailing spaces).
+// The keys of the preloaded dictionary are uppercase letters A-Z and the values are predetermined words, for example:
+// dict["P"] == "perfect"
+// Examples
+// "dgm" ==> "disturbing gregarious mustache"
+// "lkj" ==> "literal klingon joke"
+
+//My solution
+//preload variable: dict
+function makeBackronym(string) {
+   return string
+      .toUpperCase()
+      .split("")
+      .map((letter) => dict[letter])
+      .join(" ");
+}
