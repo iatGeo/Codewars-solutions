afc10b2e003855f1ec34d4a94167449c0a4c45d3
iatGeo <g.iatridis@protonmail.com>
2024-04-02 21:16:16 +0300

    more solutions

---
+++ 7kyu/exc-fire-on-the-boat.js
@@ -0,0 +1,10 @@
+// Holiday III - Fire on the boat
+// DESCRIPTION:
+// Enjoying your holiday, you head out on a scuba diving trip!
+// Disaster!! The boat has caught fire!!
+// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+//My solution
+function fireFight(s) {
+   return s.replace(/Fire/g, "~~");
+}
