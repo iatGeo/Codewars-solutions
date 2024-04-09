295a53663431b70c6183f7d19e7278ff12991e10
iatGeo <g.iatridis@protonmail.com>
2024-04-09 22:29:32 +0300

    more solutions

---
+++ 7kyu/exc-band-name-generator.js
@@ -0,0 +1,14 @@
+// Band name generator
+// DESCRIPTION:
+// My friend wants a new band name for her band. She like bands that use the formula: "The" + a noun with the first letter capitalized, for example:
+// "dolphin" -> "The Dolphin"
+// However, when a noun STARTS and ENDS with the same letter, she likes to repeat the noun twice and connect them together with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+// "alaska" -> "Alaskalaska"
+// Complete the function that takes a noun as a string, and returns her preferred band name written as a string.
+
+//My solution
+function bandNameGenerator(str) {
+   return str.slice(0, 1) == str.slice(-1)
+      ? str.slice(0, 1).toUpperCase() + str.slice(1) + str.slice(1)
+      : "The " + str.slice(0, 1).toUpperCase() + str.slice(1);
+}
