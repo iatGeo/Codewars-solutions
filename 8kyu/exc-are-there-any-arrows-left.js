f5b8c523484078513233f4ed9f19dbbe0102ea4a
iatGeo <g.iatridis@protonmail.com>
2024-04-11 22:15:28 +0300

    more solutions

---
+++ 8kyu/exc-are-there-any-arrows-left.js
@@ -0,0 +1,13 @@
+// Are there any arrows left?
+// DESCRIPTION:
+// Check your arrows
+// You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+// You need to verify that you have some good ones left, in order to prepare for battle:
+// anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
+// If an arrow in the quiver does not have a damaged status, it means it's new.
+// The expected result is a boolean, indicating whether you have any good arrows left.
+
+//My solution
+function anyArrows(arrows) {
+   return arrows.some((a) => !a.damaged);
+}
