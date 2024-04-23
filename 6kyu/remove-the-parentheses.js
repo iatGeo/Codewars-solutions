8d3f61b64374d8bf3a61a653ddfcb2d6dc9e675b
iatGeo <g.iatridis@protonmail.com>
2024-04-23 22:18:07 +0300

    more solutions

---
+++ 6kyu/remove-the-parentheses.js
@@ -0,0 +1,24 @@
+// Remove the parentheses
+// DESCRIPTION:
+// Remove the parentheses
+// In this kata you are given a string for example:
+// "example(unwanted thing)example"
+// Your task is to remove everything inside the parentheses as well as the parentheses themselves.
+// The example above would return:
+// "exampleexample"
+// Notes
+// Other than parentheses only letters and spaces can occur in the string. Don't worry about other brackets like "[]" and "{}" as these will never appear.
+// There can be multiple parentheses.
+// The parentheses can be nested.
+
+//My solution
+function removeParentheses(s) {
+   let result = "";
+   let count = 0;
+   for (let letter of s) {
+      if (letter == "(") count += 1;
+      if (count == 0) result += letter;
+      if (letter == ")") count -= 1;
+   }
+   return result;
+}
