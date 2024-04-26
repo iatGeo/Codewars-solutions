611faaa5d962481ef30f2721f870a6de2e480b94
iatGeo <g.iatridis@protonmail.com>
2024-04-26 22:18:47 +0300

    more solutions

---
+++ 7kyu/exc-vowel-one.js
@@ -0,0 +1,13 @@
+// Vowel one
+// DESCRIPTION:
+// vowelOne
+// Write a function that takes a string and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+// All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+// Examples:
+// vowelOne( "abceios" ) // "1001110"
+// vowelOne( "aeiou, abc" ) // "1111100100"
+
+//My solution
+function vowelOne(s) {
+   return s.replace(/[^aeiou]/gi, "0").replace(/[^\d]/g, "1");
+}
