b0b0c9d8eab269e48c5099176dcc0204a1ee686b
iatGeo <g.iatridis@protonmail.com>
2024-04-30 23:15:59 +0300

    more solutions

---
+++ 8kyu/enumerable-true-or-any.js
@@ -0,0 +1,8 @@
+// Enumerable Magic #2 - True for Any?
+// DESCRIPTION:
+// Create an any? (JS: any) function that accepts an array and a block (JS: function), and returns true if the block (/function) returns true for any item in the array. If the array is empty, the function should return false.
+
+//My solution
+function any(arr, fun) {
+   return arr.some(fun);
+}
