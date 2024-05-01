6cc1539b2f2412c9d3b97797578bab5ffecfc69c
iatGeo <g.iatridis@protonmail.com>
2024-05-01 22:14:28 +0300

    more solutions

---
+++ 8kyu/exc-mr-freeze.js
@@ -0,0 +1,7 @@
+// Mr. Freeze
+// DESCRIPTION:
+// There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+//My solution
+// mark the MrFreeze object instance as frozen
+Object.freeze(MrFreeze);
