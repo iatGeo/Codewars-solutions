68bdfa6470985d724b278d58b59ee0160dd2668f
iatGeo <g.iatridis@protonmail.com>
2024-05-10 23:09:01 +0300

    more solutions

---
+++ 7kyu/frequency-sequence.js
@@ -0,0 +1,15 @@
+// Frequency sequence
+// DESCRIPTION:
+// Your task is to return an output string that translates an input string s by replacing each character in s with a number representing the number of times that character occurs in s and separating each number with the sep character(s).
+// Example (s, sep --> Output)
+// "hello world", "-" --> "1-1-3-3-2-1-1-2-1-3-1"
+// "19999999"   , ":" --> "1:7:7:7:7:7:7:7"
+// "^^^**$"     , "x" --> "3x3x3x2x2x1"
+
+//My solution
+function freqSeq(str, sep) {
+   return str
+      .split("")
+      .map((elem, index, arr) => arr.filter((x) => x === elem).length)
+      .join(`${sep}`);
+}
