0f52c090e7a15879d78e9c71fd16aa1a6ed483c2
iatGeo <g.iatridis@protonmail.com>
2024-05-17 22:24:59 +0300

    more solutions

---
+++ 7kyu/last-digits-of-a-number.js
@@ -0,0 +1,32 @@
+// last digits of a number
+// DESCRIPTION:
+// Your job is to implement a function which returns the last D digits of an integer N as a list.
+// Special cases:
+// If D > (the number of digits of N), return all the digits.
+// If D <= 0, return an empty list.
+// Examples:
+// N = 1
+// D = 1
+// result = [1]
+// N = 1234
+// D = 2
+// result = [3, 4]
+// N = 637547
+// D = 6
+// result = [6, 3, 7, 5, 4, 7]
+
+//My solution
+function lastDigit(n, d) {
+   if (d > n.length) {
+      return n;
+   } else if (d <= 0) {
+      return [];
+   } else {
+      return n && String(n).length
+         ? String(n)
+              .slice(-d)
+              .split("")
+              .map((num) => +num)
+         : [0];
+   }
+}
