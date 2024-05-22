3428a1e2ee469d0aa9a88f469e9d2a2fc9b4ec3b
iatGeo <g.iatridis@protonmail.com>
2024-05-22 23:41:17 +0300

    more solutions

---
+++ 8kyu/exc-sort-textbooks.js
@@ -0,0 +1,15 @@
+// Sort My Textbooks
+// DESCRIPTION:
+// HELP! Jason can't find his textbook! It is two days before the test date, and Jason's textbooks are all out of order! Help him sort a list (ArrayList in java) full of textbooks by subject, so he can study before the test.
+// The sorting should NOT be case sensitive
+
+//My solution
+function sorter(textbooks) {
+   return textbooks.sort((a, b) =>
+      a.toLowerCase() < b.toLowerCase()
+         ? -1
+         : a.toLowerCase() > b.toLowerCase()
+         ? 1
+         : 0
+   );
+}
