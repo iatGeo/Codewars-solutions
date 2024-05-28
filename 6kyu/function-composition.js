3ccb1d30a658c62e06adb37126e8fd8268457ad7
iatGeo <g.iatridis@protonmail.com>
2024-05-28 22:27:20 +0300

    more solutions

---
+++ 6kyu/function-composition.js
@@ -0,0 +1,16 @@
+// Function Composition
+// DESCRIPTION:
+// Function composition is a mathematical operation that mainly presents itself in lambda calculus and computability. It is explained well here, but this is my explanation, in simple mathematical notation:
+// f3 = compose( f1 f2 )
+//    Is equivalent to...
+// f3(a) = f1( f2( a ) )
+// Your task is to create a compose function to carry out this task, which will be passed two functions or lambdas. Ruby functions will be passed, and should return, either a proc or a lambda. Remember that the resulting composed function may be passed multiple arguments!
+// compose(f , g)(x)
+// => f( g( x ) )
+
+//My solution
+function compose(f, g) {
+   return function () {
+      return f(g.apply(this, arguments));
+   };
+}
