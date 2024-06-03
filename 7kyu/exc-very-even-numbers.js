affdf65393132c0b735e573a9fb0bd48f1c016f4
iatGeo <g.iatridis@protonmail.com>
2024-06-03 21:19:35 +0300

    more solutions

---
+++ 7kyu/exc-very-even-numbers.js
@@ -0,0 +1,25 @@
+// "Very Even" Numbers.
+// DESCRIPTION:
+// Task
+// Write a function that returns true if the number is a "Very Even" number.
+// If a number is a single digit, then it is simply "Very Even" if it itself is even.
+// If it has 2 or more digits, it is "Very Even" if the sum of its digits is "Very Even".
+// Examples
+// number = 88 => returns false -> 8 + 8 = 16 -> 1 + 6 = 7 => 7 is odd
+// number = 222 => returns true -> 2 + 2 + 2 = 6 => 6 is even
+// number = 5 => returns false
+// number = 841 => returns true -> 8 + 4 + 1 = 13 -> 1 + 3 => 4 is even
+// Note: The numbers will always be 0 or positive integers!
+
+//My solution
+function isVeryEvenNumber(n) {
+   if (String(n).length === 1) {
+      return n % 2 === 0 ? true : false;
+   } else {
+      let num = String(n);
+      while (num.length > 1) {
+         num = String(num.split("").reduce((a, b) => a + +b, 0));
+      }
+      return +num % 2 === 0 ? true : false;
+   }
+}
