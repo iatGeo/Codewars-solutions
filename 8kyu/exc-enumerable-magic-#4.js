b9fc76c96d5475b5aad2aa8759758552134faf10
iatGeo <g.iatridis@protonmail.com>
2024-06-04 22:39:11 +0300

    more solutions

--- 8kyu/exc-enumerable-magic-#4.js
+++ 8kyu/exc-enumerable-magic-#4.js
@@ -3,24 +3,6 @@
 // Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
 
 //My solution
-//original Class
-// class Cube {
-//   getSide() {
-//     return this.side;
-//   }
-//   setSide(n) {
-//     this.side = Math.abs(n);
-//   }
-// }
-
-class Cube {
-   constructor(side = 0) {
-      this.side = side;
-   }
-   getSide() {
-      return Math.abs(this.side);
-   }
-   setSide(side) {
-      this.side = side;
-   }
+function none(arr, fun) {
+   return !arr.some(fun);
 }
